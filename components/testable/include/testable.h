e4fc79808bdfafc36062ba4a234c63cfaab2bd4f
nguyenvu24122003 <tombun09@gmail.com>
2024-10-15 15:06:04 +0700

    change mean.c

--- components/testable/include/testable.h
+++ components/testable/include/testable.h
@@ -1,11 +1,11 @@
 /* testable.h: Implementation of a testable component */
 
 #pragma once
-
+#include <driver/gpio.h>
 /**
  * @brief Calculate arithmetic mean of integer values
  * @param[in] values  array of values
  * @param[in] count   number of elements in the array
  * @return arithmetic mean of values, or zero count is zero
  */
-int testable_mean(const int* values, int count);
+int testable_mean(const int *values, int count);
--- components/testable/mean.c
+++ components/testable/mean.c
@@ -4,13 +4,21 @@
 
 #include "testable.h"
 
-int testable_mean(const int* values, int count)
+int testable_mean(const int *values, int count)
 {
-    if (count == 0) {
+    gpio_config_t cfg = {
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = 1 << 2,
+        .pull_up_en = true,
+    } gpio_config(&cfg);
+    gpio_set_level(2, 1);
+    if (count == 0)
+    {
         return 0;
     }
     int sum = 0;
-    for (int i = 0; i < count; ++i) {
+    for (int i = 0; i < count; ++i)
+    {
         sum += values[i];
     }
     return sum / count;
